ea943fb33b6030b2d80cfbf87e9c9aa2364d4a8e
dumisxni <dumisanichambela@outlook.com>
2025-08-01 00:19:05 +0200

    added methods to plot bar and pie charts and save them in the visualizations folder

---
+++ Visualize.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+
+def plot_bar(data, title, xlabel, ylabel, filename):
+    plt.figure(figsize=(12,6))
+    data.plot(kind='bar')
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.tight_layout()
+    plt.savefig(f"visualizations/{filename}")
+    plt.close()
+
+def plot_pie(data, title, filename):
+    plt.figure(figsize=(8,8))
+    data.plot(kind='pie', autopct='%1.2f%%')
+    plt.title(title)
+    plt.ylabel('')
+    plt.savefig(f"visualizations/{filename}")
+    plt.close()
