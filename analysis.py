d567ff1531ad38dac3061a5bdd187602a4bc137c
dumisxni <dumisanichambela@outlook.com>
2025-07-31 09:37:14 +0200

    dropped all empty columns and saved the dataframe as csv

--- analysis.py
+++ analysis.py
@@ -7,3 +7,6 @@ df = pd.read_csv('C:/Users/sdumz/Desktop/domaintel/data/whois-db-download-info-s
 new_df = df.dropna(axis=1, how='all').copy()
 new_df = new_df.drop(['registrant_fax','administrativeContact_fax'], axis=1)
 new_df['registrant_organization'] = new_df['registrant_organization'].fillna('unknown')
+new_df = new_df.dropna()
+# print(new_df.head(1000).to_string())
+new_df.to_csv('C:/Users/sdumz/Desktop/domaintel/data/whois-db-download-info-cleaned.csv')
